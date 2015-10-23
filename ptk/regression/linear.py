06ef49f85cf66e9e90df620e252f53d0e9a2bf5e
Peter Schulam <pschulam@gmail.com>
2015-10-23 10:48:28 -0400

    Add mle function linear regression module.

--- ptk/regression/linear.py
+++ ptk/regression/linear.py
@@ -3,5 +3,12 @@ import numpy as np
 from scipy import linalg
 
 
+def penalized_mle(X, y, P):
+    ss1, ss2 = sufficient_stats(X, y)
+    ss1 += P
+    return linalg.solve(ss1, ss2)
+
+
 def sufficient_stats(X, y):
     return np.dot(X.T, X), np.dot(X.T, y)
+
